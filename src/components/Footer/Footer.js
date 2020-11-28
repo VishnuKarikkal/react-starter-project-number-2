257dae93ca5f2b6f7b7f41bad78915a32c3f6a4e
VishnuKarikkal <sajanvishnu0@gmail.com>
2020-11-28 20:12:56 +0530

    page modifications

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../css/footer.css';
+import {FaFacebook, FaInstagram, FaGithub} from 'react-icons/fa';
 
 function Footer() {
     return (
@@ -33,9 +34,9 @@ function Footer() {
                     <div className="col-6 col-sm-6 col-lg-3">
                     <h5 className="py-2">Social Media</h5>
                         <ul>
-                            <li>facebook</li>
-                            <li>instagram</li>
-                            <li>github</li>
+                            <li>facebook &nbsp; <FaFacebook /> </li>
+                            <li>instagram &nbsp; <FaInstagram /> </li>
+                            <li>github &nbsp; <FaGithub /> </li>
                         </ul>
                     </div>
                 </div>
@@ -43,8 +44,11 @@ function Footer() {
                     <div className="col-4">
                         <h2>🤩🤩</h2>
                     </div>
-                    <div className="col-6 col-sm-6 align-self-end">
+                    <div className="col-5 col-sm-5">
                         <h6> &copy; 2020</h6>
+                    </div>
+                    <div className="col-2">
+                    <FaFacebook /> &nbsp; <FaInstagram /> &nbsp; <FaGithub />
                     </div>
                 </div>
            
